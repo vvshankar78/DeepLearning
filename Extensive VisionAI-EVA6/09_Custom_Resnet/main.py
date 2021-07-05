8577ce8909673db6c1a143a85b3c85ad07d441f0
vidhya shankar <vvshankar78@gmail.com>
2021-07-05 07:03:32 +0530

    main

--- Extensive VisionAI-EVA6/09_Custom_Resnet/main.py
+++ Extensive VisionAI-EVA6/09_Custom_Resnet/main.py
@@ -2,7 +2,7 @@ import numpy as np
 import argparse
 
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
-parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
+parser.add_argument('--lr', default=1, type=float, help='learning rate')
 parser.add_argument('--resume', '-r', action='store_true',
                     help='resume from checkpoint')
 args = parser.parse_args()
