91a9eccb1e472d2f8183ae3033a11d9ad6a65a1e
vidhya shankar <vvshankar78@gmail.com>
2021-07-06 06:31:45 +0530

    data transforms

--- Extensive VisionAI-EVA6/09_Custom_Resnet/data/data_transforms.py
+++ Extensive VisionAI-EVA6/09_Custom_Resnet/data/data_transforms.py
@@ -23,15 +23,15 @@ def albumentations_transforms(p=1.0, is_train=False):
 	# Use data aug only for train data
 	if is_train:
 		transforms_list.extend([
-			HorizontalFlip(p=0.5),
-            ShiftScaleRotate(shift_limit=0.1, scale_limit=0.1, rotate_limit=15, p=0.5),
-            PadIfNeeded(min_height=32, min_width=32, border_mode=BORDER_CONSTANT, value=mean*255.0, p=1.0),
+			# HorizontalFlip(p=0.5),
+            # ShiftScaleRotate(shift_limit=0.1, scale_limit=0.1, rotate_limit=15, p=0.5),
+            PadIfNeeded(min_height=38, min_width=38, border_mode=BORDER_CONSTANT, value=mean*255.0, p=1.0),
 			OneOf([
-				#RandomCrop(height=32, width=32, p=0.8),
+				RandomCrop(height=38, width=38, p=0.8),
 				#CenterCrop(height=32, width=32, p=0.2),
-                CoarseDropout(max_holes=1, min_holes=1, max_height=16, max_width=16, min_height=16,
-						min_width=16, fill_value=mean*255.0, mask_fill_value=None, p=0.7),
-                ToGray(p=0.3),
+                CoarseDropout(max_holes=1, min_holes=1, max_height=8, max_width=8, min_height=8,
+						min_width=8, fill_value=mean*255.0, mask_fill_value=None, p=0.7),
+                # ToGray(p=0.3),
 			], p=1.0),
 			
 			
