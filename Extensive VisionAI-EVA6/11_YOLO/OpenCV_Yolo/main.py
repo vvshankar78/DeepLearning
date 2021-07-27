9c7fcd1be0ceffea2c263482dcde297d7c593876
vidhya shankar <vvshankar78@gmail.com>
2021-07-27 17:41:14 +0530

    what ever

--- Extensive VisionAI-EVA6/11_YOLO/OpenCV_Yolo/main.py
+++ Extensive VisionAI-EVA6/11_YOLO/OpenCV_Yolo/main.py
@@ -3,12 +3,30 @@ import numpy as np
 import urllib.request
 
 
-
 class RunYolo:
     def __init__(self, url, path):
         self.url = url
         self.path = path
+        self.download_weight()
+        self.net = self.load_network()
+        self.classes = self.load_classes()
+
 
     def download_weight(self):
         path = self.path +  "/yolov3.weights"
+        print('begining download')
         urllib.request.urlretrieve(self.url, path)
+        print('download completed')
+
+    def load_network(self):
+        print('loading yolo network')
+        net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
+        return net
+
+    def load_classes(self):
+        classes = []
+        print('loading classes')
+        with open("coco.names", "r") as f:
+            classes = [line.strip() for line in f.readlines()]
+        print(classes)
+        return classes
